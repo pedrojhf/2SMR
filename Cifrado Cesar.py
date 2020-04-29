a3ec69a2ff510613fa6edcb803d6f208ba3c0885
Pedro <pedroherasfp@gmail.com>
2020-04-29 11:34:49 +0200

    Actualización cifrado cesar con parte descifrado

--- Cifrado Cesar.py
+++ Cifrado Cesar.py
@@ -18,5 +18,12 @@ for letra in texto_claro:
     nueva_posicion = abecedario.find(letra) + clave
     letra_cifrada = int(nueva_posicion) % len(abecedario)
     texto_cifrado = texto_cifrado + str(abecedario[letra_cifrada])
-
 print("\nEl mensaje cifrado es:",texto_cifrado)
+texto_descifrado=""
+
+for letra in texto_cifrado:
+    nueva_posicion = abecedario.find(letra) - clave
+    letra_cifrada = int(nueva_posicion) % len(abecedario)
+    texto_descifrado = texto_descifrado + str(abecedario[letra_cifrada])
+
+print("\nEl mensaje descifrado es:",texto_descifrado)
