471cb987597effed0c44d560d2d4060373df5d98
Pedro <pedroherasfp@gmail.com>
2020-04-29 11:20:15 +0200

    Añado el Cifrado Cesar al repositorio

---
+++ Cifrado Cesar.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 10:53:41 2020
+
+@author: Pedro
+"""
+abecedario = 'abcdefghijklmnopqrstuvwxyz'
+
+print("BIENVENIDO A MI CIFRADOR CÉSAR")
+
+texto_claro=input("Escribe el texto a cifrar:")
+clave=int(input("Escribe la clave de cifrado (un número del 1 al 27):"))
+
+
+texto_cifrado=""
+
+for letra in texto_claro:
+    nueva_posicion = abecedario.find(letra) + clave
+    letra_cifrada = int(nueva_posicion) % len(abecedario)
+    texto_cifrado = texto_cifrado + str(abecedario[letra_cifrada])
+
+print("\nEl mensaje cifrado es:",texto_cifrado)
