0261e0d6716998f35c2cce2cd75ee78e4ff4b17e
Pedro <pedroherasfp@gmail.com>
2020-04-30 03:48:19 +0200

    Ejercicios 1-7

--- Cifrado Cesar.py
+++ Cifrado Cesar.py
@@ -11,19 +11,26 @@ print("BIENVENIDO A MI CIFRADOR CÉSAR")
 texto_claro=input("Escribe el texto a cifrar:")
 clave=int(input("Escribe la clave de cifrado (un número del 1 al 27):"))
 
+def cifracesar(texto_claro,clave):
+    texto_cifrado=""
 
-texto_cifrado=""
+    for letra in texto_claro:
+        nueva_posicion = abecedario.find(letra) + clave
+        letra_cifrada = int(nueva_posicion) % len(abecedario)
+        texto_cifrado = texto_cifrado + str(abecedario[letra_cifrada])
+    return texto_cifrado
+cifrado=cifracesar(texto_claro,clave)
+print("\nEl mensaje descifrado es:",cifrado)   
 
-for letra in texto_claro:
-    nueva_posicion = abecedario.find(letra) + clave
-    letra_cifrada = int(nueva_posicion) % len(abecedario)
-    texto_cifrado = texto_cifrado + str(abecedario[letra_cifrada])
-print("\nEl mensaje cifrado es:",texto_cifrado)
-texto_descifrado=""
 
-for letra in texto_cifrado:
-    nueva_posicion = abecedario.find(letra) - clave
-    letra_cifrada = int(nueva_posicion) % len(abecedario)
-    texto_descifrado = texto_descifrado + str(abecedario[letra_cifrada])
+def descifracesar(texto_cifrado,clave):
+    texto_descifrado=""
 
-print("\nEl mensaje descifrado es:",texto_descifrado)
+    for letra in texto_cifrado:
+        nueva_posicion = abecedario.find(letra) - clave
+        letra_cifrada = int(nueva_posicion) % len(abecedario)
+        texto_descifrado = texto_descifrado + str(abecedario[letra_cifrada])
+ 
+    return texto_descifrado
+descifrado=descifracesar(cifrado,clave)
+print("\nEl mensaje descifrado es:",descifrado)   
